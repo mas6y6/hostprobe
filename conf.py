438069c7ad2b5f4de880dd2a949152646424d297
malachi196 <malachiaaronwilson@gmail.com>
2024-11-25 10:17:01 -0600

    added sphinx config

---
+++ conf.py
@@ -0,0 +1,29 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from .hostprobe._version import __version__ as _v
+
+project = 'hostprobe'
+copyright = '2024, malachi196'
+author = 'malachi196'
+version = _v
+
+# -- General configuration ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+]
+extensions = []
+language = 'en'
+master_doc = 'index'
+pygments_style = 'sphinx'
+source_suffix = '.rst'
+templates_path = ['_templates']
+
+# -- Options for HTML output ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
