71e2711596569b5cf9e46a6025b9e4f8f76a302b
malachi196 <malachiaaronwilson@gmail.com>
2024-11-25 16:08:20 -0600

    add remake docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,11 +30,11 @@ language = 'en'
 master_doc = 'index'
 pygments_style = 'sphinx'
 source_suffix = '.rst'
-templates_path = ['_templates']
+templates_path = ['./_templates']
 
 # -- Options for HTML output ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ['./_static']
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
