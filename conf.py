1314cf896ba10a431d03ecd9c697a3f5fb592907
malachi196 <malachiaaronwilson@gmail.com>
2024-11-25 13:59:37 -0600

    fixed conf

--- conf.py
+++ conf.py
@@ -21,10 +21,10 @@ language = 'en'
 master_doc = 'index'
 pygments_style = 'sphinx'
 source_suffix = '.rst'
-templates_path = ['_templates']
+templates_path = ['dovs/_templates']
 
 # -- Options for HTML output ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ['docs/_static']
