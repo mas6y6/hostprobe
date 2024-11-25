e7e8b3bae9abc44bfb108236ff324df71f6be02f
malachi196 <malachiaaronwilson@gmail.com>
2024-11-25 08:38:21 -0600

    create hostprobe

---
+++ README.md
@@ -0,0 +1,6 @@
+# hostprobe
+a cross-platform toolkit for probing networks in python
+
+&nbsp;
+
+created by @malachi196
---
+++ hostprobe/__init__.py
@@ -0,0 +1,10 @@
+from ._hostprobe import netprobe, check_host, memory_usage
+from ._constants import DEFAULTTHRESHOLD, MINTHRESHOLD
+
+__all__=(
+    "netprobe",
+    "check_host",
+    "DEFAULTTHRESHOLD",
+    "MINTHRESHOLD",
+    "memory_usage",
+)
