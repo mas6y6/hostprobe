011bf8361983e9af14a3db68d33b61af8375009e
malachi196 <malachiaaronwilson@gmail.com>
2024-11-25 16:15:27 -0600

    fix rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,6 @@ templates_path = ['./_templates']
 # -- Options for HTML output ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['./_static']
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
